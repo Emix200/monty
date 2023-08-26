6941581d27d809a9be1e7316dc61d2b522fd644d
emix200 <aleajiemix@gmail.com>
2023-08-25 19:56:01 -0700

    stack project partially done

---
+++ queue.c
@@ -0,0 +1,49 @@
+#include "monty.h"
+
+/**
+ * f_queue - prints out stack from the top
+ * @head: head of stack
+ * @counter: line_number
+ * Return: --
+ */
+void f_queue(stack_t **head, unsigned int counter)
+{
+	(void)head;
+	(void)counter;
+	bus.lifi = 1;
+}
+
+/**
+ * addqueue - add node to stack
+ * @n: new value
+ * @head: head of stack
+ * Return: --
+ */
+void addqueue(stack_t **head, int n)
+{
+	stack_t *new_node, *aux;
+
+	aux = *head;
+	new_node = malloc(sizeof(stack_t));
+	if (new_node == NULL)
+	{
+		printf("Error\n");
+	}
+	new_node->n = n;
+	new_node->next = NULL;
+	if (aux)
+	{
+		while (aux->next)
+			aux = aux->next;
+	}
+	if (!aux)
+	{
+		*head = new_node;
+		new_node->prev = NULL;
+	}
+	else
+	{
+		aux->next = new_node;
+		new_node->prev = aux;
+	}
+}
---
+++ stack.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+
+/**
+ * f_stack - sets the format of the data to a stack (LIFO).
+ * @head: hesd of stack
+ * @counter: LINE_NUMBER
+ * Return: --
+ */
+void f_stack(stack_t **head, unsigned int counter)
+{
+	(void)head;
+	(void)counter;
+	bus.lifi = 0;
+}
